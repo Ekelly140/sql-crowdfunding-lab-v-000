5baf45fb29d58ea6999ad2ed1a17dc482999e5cc
ERIC KELLY <eakelly220@gmail.com>
2018-06-19 19:25:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,8 +14,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, pledges.amount FROM users
- RIGHT JOIN pledges ON pledges.user_ID = users.id
+"SELECT users.name, users.age, pledges.amount FROM pledges
+ LEFT JOIN users ON pledges.user_ID = users.id
  GROUP BY users.name
 "
 end
