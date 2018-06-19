2e0018a573f1fc2326fe38f22455a6bf78a6760b
ERIC KELLY <eakelly220@gmail.com>
2018-06-19 17:57:03 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,9 +2,9 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  goal, INTEGER,
-  start_date, TEXT,
-  end_date, TEXT
+  goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
   );
 
 CREATE TABLE users (
